67b1c236e95cf8b855d8cd76ccff68a5b2e37ab9
Felip8 <valdivinobiava496@gmail.com>
2025-03-01 17:32:55 -0300

    teste

---
+++ src/app.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class app {
+
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        int op;
+        do{
+            System.out.println(" [1] - Calcular Preço Total / [2] - Calcular Troco / [3] - Sair ");
+            op = scan.nextInt();
+
+            switch(op){
+                case 1:
+
+            }
+        }while(op != 3);
+    }
+}
